541f91111c2ff0fe08673c31d3804c44159992d4
Rafael Azevedo <rafaelazevedo321@gmail.com>
2026-03-19 18:45:27 -0300

    fix: validation lenght certificates additionals

--- src/sections/Courses/index.tsx
+++ src/sections/Courses/index.tsx
@@ -48,46 +48,55 @@ import frontEndCertificatePdf from "../../assets/certificates/pdf/FrontEnd.pdf";
 import fundamentalsCertificatePdf from "../../assets/certificates/pdf/Fundamentals.pdf";
 import mainAiFullcycleCertificatePdf from "../../assets/certificates/pdf/AiFullCycle.pdf";
 
-export const Courses = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const courses = [
-    {
-      school: "FullCycle",
-      logo: aiFullcycleLogo,
-      name: "Desenvolvimento com IA",
-      startDate: "Mar 2026",
-      endDate: "Mar 2026",
-      qtdHours: 8,
-      status: "Concluído",
-      contents: [
-        "Prompt e Context Engineering",
-        "Integrar aplicações usando IA (vai muinto além de usar SDK da OpenAI)",
-        "Criar Agentes de IA",
-        "Criar skills para agentes IA",
-        "Problemas que não existiam e que a IA nos trouxe",
-        "Entendimento aprofundado sobre o que são agentes de IA",
-        "Gerenciamento de prompts e o impacto nos agentes",
-        "Retrieval Augmented Generation (RAG)",
-        "Frameworks para desenvolvimento de agentes",
-      ],
-      mainCertificate: {
-        image: mainAiFullcycleCertificateImage,
-        name: "Desenvolvimento Web",
-        link: "",
-        pdf: mainAiFullcycleCertificatePdf,
-        contents: [],
-      },
-      certificatesAdditionals: [],
+const courses = [
+  {
+    school: "FullCycle",
+    logo: aiFullcycleLogo,
+    name: "Desenvolvimento com IA",
+    startDate: "Mar 2026",
+    endDate: "Mar 2026",
+    qtdHours: 8,
+    status: "Concluído",
+    contents: [
+      "Prompt e Context Engineering",
+      "Integrar aplicações usando IA (vai muinto além de usar SDK da OpenAI)",
+      "Criar Agentes de IA",
+      "Criar skills para agentes IA",
+      "Problemas que não existiam e que a IA nos trouxe",
+      "Entendimento aprofundado sobre o que são agentes de IA",
+      "Gerenciamento de prompts e o impacto nos agentes",
+      "Retrieval Augmented Generation (RAG)",
+      "Frameworks para desenvolvimento de agentes",
+    ],
+    mainCertificate: {
+      image: mainAiFullcycleCertificateImage,
+      name: "Desenvolvimento Web",
+      link: "",
+      pdf: mainAiFullcycleCertificatePdf,
+      contents: [],
     },
-    {
-      school: "Trybe",
-      logo: trybeLogo,
-      name: "Desenvolvimento Web Full Stack",
-      startDate: "Nov 2020",
-      endDate: "Nov 2021",
-      qtdHours: 1500,
-      status: "Concluído",
+    certificatesAdditionals: [],
+  },
+  {
+    school: "Trybe",
+    logo: trybeLogo,
+    name: "Desenvolvimento Web Full Stack",
+    startDate: "Nov 2020",
+    endDate: "Nov 2021",
+    qtdHours: 1500,
+    status: "Concluído",
+    contents: [
+      "Fundamentos do Desenvolvimento Web",
+      "Desenvolvimento Front-End",
+      "Desenvolvimento Back-End",
+      "Ciência da Computação",
+      "Soft Skills",
+    ],
+    mainCertificate: {
+      image: mainCertificateImage,
+      name: "Desenvolvimento Web",
+      link: "https://www.credential.net/b41dae45-7403-476c-93a6-f11209ef4474#acc.hkaCTcv9",
+      pdf: mainCertificatePdf,
       contents: [
         "Fundamentos do Desenvolvimento Web",
         "Desenvolvimento Front-End",
@@ -95,89 +104,80 @@ export const Courses = () => {
         "Ciência da Computação",
         "Soft Skills",
       ],
-      mainCertificate: {
-        image: mainCertificateImage,
-        name: "Desenvolvimento Web",
-        link: "https://www.credential.net/b41dae45-7403-476c-93a6-f11209ef4474#acc.hkaCTcv9",
-        pdf: mainCertificatePdf,
+    },
+
+    certificatesAdditionals: [
+      {
+        image: computerScienceCertificateImage,
+        name: "Ciência da Computação",
+        link: "https://www.credential.net/2c4b6b78-67b2-4a93-b11a-f7a09e0104d4#acc.skMBFx2T",
+        pdf: computerScienceCertificatePdf,
         contents: [
-          "Fundamentos do Desenvolvimento Web",
-          "Desenvolvimento Front-End",
-          "Desenvolvimento Back-End",
-          "Ciência da Computação",
-          "Soft Skills",
+          "Introdução à Python",
+          "Programação Orientada a Objetos (POO)",
+          "Redes e Raspagem de dados",
+          "Algoritmos",
+          "Estritura de Dados I: Arrays, Hashmaps e Sets",
+          "Estrutura de Dados II: Listas, Filas and Pilhas",
         ],
       },
+      {
+        image: backEndCertificateImage,
+        name: "Desenvolvimento Back-End",
+        link: "https://www.credential.net/93d37420-a5d5-4525-9597-f9d4dce703f0#acc.4uVKwoMm",
+        pdf: backEndCertificatePdf,
+        contents: [
+          "Introdução à SQL, funções SQL e JOINs",
+          "Normalização e Modelagem de Banco de Dados",
+          "Introdução a MongoDB",
+          "MongoDB - Upadates simples e complexo",
+          "MongoDB - Aggregation Framework",
+          "Desenvolvimento Web com NodeJs",
+          "NodeJs - Arquitetura MSC, Rest e Restful",
+          "Autenticação e Upload de arquivos",
+          "Deployment",
+          "Arquitetura SOLID e ORM",
+          "Sockets",
+        ],
+      },
+      {
+        image: frontEndCertificateImage,
+        name: "Desenvolvimento Front-End",
+        link: "https://www.credential.net/ac89d79b-ddd2-484a-87b0-4697f37ff20a#acc.NpCyBrFc",
+        pdf: frontEndCertificatePdf,
+        contents: [
+          "Introdução a React",
+          "Estado dos componentes, Eventos e Formulários com React",
+          "Ciclo de vida dos componentes e React Router",
+          "Metodologias ágeis",
+          "Testes automatizados com React Testing Library(RTL)",
+          "Gerenciamento de estado com Redux",
+          "Context API e React Hooks",
+        ],
+      },
+      {
+        image: fundamentalsCertificateImage,
+        name: "Fundamentos do Desenvolvimento Web",
+        link: "https://www.credential.net/fd1a5519-756f-48da-9d64-d449f0eaf0f7#acc.b4VLXrdH",
+        pdf: fundamentalsCertificatePdf,
+        contents: [
+          "Unix e Bash",
+          "Git, GitHub e internet",
+          "Introdução a HTML e CSS",
+          "JavaScript - DOM, Eventos e Web Storage",
+          "HTML e CSS - Formulários, Flexbox e Responsividade",
+          "Introdução a JavaScript ES6 e teste unitário",
+          "Funções de alta ordem de JavaScript ES6",
+          "JavaScript assíncrono e promises",
+          "Testes automatizados com Jest",
+        ],
+      },
+    ],
+  },
+];
 
-      certificatesAdditionals: [
-        {
-          image: computerScienceCertificateImage,
-          name: "Ciência da Computação",
-          link: "https://www.credential.net/2c4b6b78-67b2-4a93-b11a-f7a09e0104d4#acc.skMBFx2T",
-          pdf: computerScienceCertificatePdf,
-          contents: [
-            "Introdução à Python",
-            "Programação Orientada a Objetos (POO)",
-            "Redes e Raspagem de dados",
-            "Algoritmos",
-            "Estritura de Dados I: Arrays, Hashmaps e Sets",
-            "Estrutura de Dados II: Listas, Filas and Pilhas",
-          ],
-        },
-        {
-          image: backEndCertificateImage,
-          name: "Desenvolvimento Back-End",
-          link: "https://www.credential.net/93d37420-a5d5-4525-9597-f9d4dce703f0#acc.4uVKwoMm",
-          pdf: backEndCertificatePdf,
-          contents: [
-            "Introdução à SQL, funções SQL e JOINs",
-            "Normalização e Modelagem de Banco de Dados",
-            "Introdução a MongoDB",
-            "MongoDB - Upadates simples e complexo",
-            "MongoDB - Aggregation Framework",
-            "Desenvolvimento Web com NodeJs",
-            "NodeJs - Arquitetura MSC, Rest e Restful",
-            "Autenticação e Upload de arquivos",
-            "Deployment",
-            "Arquitetura SOLID e ORM",
-            "Sockets",
-          ],
-        },
-        {
-          image: frontEndCertificateImage,
-          name: "Desenvolvimento Front-End",
-          link: "https://www.credential.net/ac89d79b-ddd2-484a-87b0-4697f37ff20a#acc.NpCyBrFc",
-          pdf: frontEndCertificatePdf,
-          contents: [
-            "Introdução a React",
-            "Estado dos componentes, Eventos e Formulários com React",
-            "Ciclo de vida dos componentes e React Router",
-            "Metodologias ágeis",
-            "Testes automatizados com React Testing Library(RTL)",
-            "Gerenciamento de estado com Redux",
-            "Context API e React Hooks",
-          ],
-        },
-        {
-          image: fundamentalsCertificateImage,
-          name: "Fundamentos do Desenvolvimento Web",
-          link: "https://www.credential.net/fd1a5519-756f-48da-9d64-d449f0eaf0f7#acc.b4VLXrdH",
-          pdf: fundamentalsCertificatePdf,
-          contents: [
-            "Unix e Bash",
-            "Git, GitHub e internet",
-            "Introdução a HTML e CSS",
-            "JavaScript - DOM, Eventos e Web Storage",
-            "HTML e CSS - Formulários, Flexbox e Responsividade",
-            "Introdução a JavaScript ES6 e teste unitário",
-            "Funções de alta ordem de JavaScript ES6",
-            "JavaScript assíncrono e promises",
-            "Testes automatizados com Jest",
-          ],
-        },
-      ],
-    },
-  ];
+export const Courses = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Section id="courses">
@@ -223,7 +223,7 @@ export const Courses = () => {
                   </ContentsList>
                 </CourseInfo>
 
-                {mainCertificate && (
+                {!!mainCertificate && (
                   <MainCertificate>
                     <MainCertificateImage src={mainCertificate.image} />
 
@@ -244,7 +244,7 @@ export const Courses = () => {
                   </MainCertificate>
                 )}
               </CourseContent>
-              {certificatesAdditionals.length && (
+              {!!certificatesAdditionals.length && (
                 <>
                   <ToggleWrapper>
                     <ToggleButton onClick={() => setIsOpen(!isOpen)}>
